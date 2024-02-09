bfec10d8754ed884474c5edfdd5001d23bbc5105
priyammazumdar <priyammazumdar1@gmail.com>
2024-02-09 08:37:49 -0600

    added in num_params

--- pipeline/config.py
+++ pipeline/config.py
@@ -9,12 +9,6 @@ class Config:
     sample_rate: int = 16000
     path_to_pretrained_models: str = "models/"
 
-    # model_config: dict = {"sew": {
-    #     {"model_config": "patrickvonplaten/sew-small-100k-timit", "params": 89644350},
-    #     {"model_config": "patrickvonplaten/sew-mid-100k-librispeech-clean-100h-ft" 174698814}
-    #     }, 
-    # }
-
     model_catalog: dict = {"sew": {"processor": AutoProcessor, 
                                   "model": SEWForCTC, 
                                   "configs": [
@@ -25,98 +19,75 @@ class Config:
                            "sewd": {"processor": AutoProcessor, 
                                   "model": SEWDForCTC, 
                                   "configs": [
-                                      {"model_config": "asapp/sew-d-tiny-100k-ft-ls100h", "params": 89644350},
-                                      {"model_config": "asapp/sew-d-mid-k127-400k-ft-ls100h", "params": 174698814},
-                                      {"model_config": "asapp/sew-d-base-plus-400k-ft-ls100h", "params": 174698814}
+                                      {"model_config": "asapp/sew-d-tiny-100k-ft-ls100h", "params": 24127423},
+                                      {"model_config": "asapp/sew-d-mid-k127-400k-ft-ls100h", "params": 80389023},
+                                      {"model_config": "asapp/sew-d-base-plus-400k-ft-ls100h", "params": 177003711}
                                   ]},
 
                            "speech2text": {"processor": Speech2TextProcessor, 
                                   "model": Speech2TextForConditionalGeneration, 
                                   "configs": [
-                                      {"model_config": "facebook/s2t-small-librispeech-asr", "params": 89644350},
-                                      {"model_config": "facebook/s2t-medium-librispeech-asr", "params": 174698814},
-                                      {"model_config": "facebook/s2t-large-librispeech-asr", "params": 174698814}
+                                      {"model_config": "facebook/s2t-small-librispeech-asr", "params": 31335424},
+                                      {"model_config": "facebook/s2t-medium-librispeech-asr", "params": 74806272},
+                                      {"model_config": "facebook/s2t-large-librispeech-asr", "params": 275031040}
                                   ]},
 
                            "speecht5": {"processor": SpeechT5Processor, 
                                   "model": SpeechT5ForSpeechToText, 
                                   "configs": [
-                                      {"model_config": "microsoft/speecht5_asr", "params": 89644350},
+                                      {"model_config": "microsoft/speecht5_asr", "params": 154588800},
                                   ]},
 
                            "unispeech": {"processor": AutoProcessor, 
                                   "model": UniSpeechForCTC, 
                                   "configs": [
-                                      {"model_config": "patrickvonplaten/unispeech-large-1500h-cv-timit", "params": 89644350}
+                                      {"model_config": "patrickvonplaten/unispeech-large-1500h-cv-timit", "params": 315470495}
                                   ]},
 
                            "unispeechsat": {"processor": AutoProcessor, 
                                   "model": UniSpeechSatForCTC, 
                                   "configs": [
-                                      {"model_config": "microsoft/unispeech-sat-base-100h-libri-ft", "params": 89644350}
+                                      {"model_config": "microsoft/unispeech-sat-base-100h-libri-ft", "params": 94396320}
                                   ]},
 
                            "wav2vec2": {"processor": AutoProcessor, 
                                   "model": Wav2Vec2ForCTC, 
                                   "configs": [
-                                      {"model_config": "facebook/wav2vec2-base-960h", "params": 89644350},
-                                      {"model_config": "facebook/wav2vec2-large-960h", "params": 174698814}
+                                      {"model_config": "facebook/wav2vec2-base-960h", "params": 94396320},
+                                      {"model_config": "facebook/wav2vec2-large-960h", "params": 315461792}
                                   ]},
 
                             
                            "xlsr": {"processor": AutoProcessor, 
                                   "model": Wav2Vec2ForCTC, 
                                   "configs": [
-                                      {"model_config": "jonatasgrosman/wav2vec2-large-xlsr-53-english", "params": 89644350},
+                                      {"model_config": "jonatasgrosman/wav2vec2-large-xlsr-53-english", "params": 315472545},
                                   ]},
 
 
                            "wavlm": {"processor": AutoProcessor, 
                                   "model": WavLMForCTC, 
                                   "configs": [
-                                      {"model_config": "patrickvonplaten/wavlm-libri-clean-100h-base", "params": 89644350},
-                                      {"model_config": "patrickvonplaten/wavlm-libri-clean-100h-large", "params": 174698814}
+                                      {"model_config": "patrickvonplaten/wavlm-libri-clean-100h-base", "params": 94405775},
+                                      {"model_config": "patrickvonplaten/wavlm-libri-clean-100h-large", "params": 315484895}
                                   ]},
 
                            "whisper": {"processor": WhisperProcessor, 
                                   "model": WhisperForConditionalGeneration, 
                                   "configs": [
-                                      {"model_config": "openai/whisper-tiny", "params": 89644350},
-                                      {"model_config": "openai/whisper-small", "params": 89644350},
-                                      {"model_config": "openai/whisper-medium", "params": 89644350},
-                                      {"model_config": "openai/whisper-base", "params": 89644350},
-                                      {"model_config": "openai/whisper-large-v2", "params": 89644350}
+                                      {"model_config": "openai/whisper-tiny", "params": 37760640},
+                                      {"model_config": "openai/whisper-small", "params": 241734912},
+                                      {"model_config": "openai/whisper-medium", "params": 763857920},
+                                      {"model_config": "openai/whisper-base", "params": 72593920},
+                                      {"model_config": "openai/whisper-large-v2", "params": 1543304960}
                                   ]},
 
                            "conformer": {"processor": AutoProcessor, 
                                   "model": Wav2Vec2ConformerForCTC, 
                                   "configs": [
-                                      {"model_config": "facebook/wav2vec2-conformer-rel-pos-large-960h-ft", "params": 89644350},
-                                      {"model_config": "facebook/wav2vec2-conformer-rope-large-960h-ft", "params": 174698814},
-                                      {"model_config": "facebook/wav2vec2-conformer-rel-pos-large-100h-ft", "params": 89644350},
-                                      {"model_config": "facebook/wav2vec2-conformer-rope-large-100h-ft", "params": 89644350}
+                                      {"model_config": "facebook/wav2vec2-conformer-rel-pos-large-960h-ft", "params": 618591904},
+                                      {"model_config": "facebook/wav2vec2-conformer-rope-large-960h-ft", "params": 593376928},
+                                      {"model_config": "facebook/wav2vec2-conformer-rel-pos-large-100h-ft", "params": 618591904},
+                                      {"model_config": "facebook/wav2vec2-conformer-rope-large-100h-ft", "params": 593376928}
                                   ]},
                          }
-    
-
-                        #   "sewd": sewd_config,
-                        #   "speech2text": speech2text_config, 
-                        #   "unispeech": unispeech_config,
-                        #   "unispeechsat": unispeechsat_config,
-                        #   "wav2vec2": wav2vec2_config,
-                        #   "conformer": conformer_config, 
-                        #   "wavlm": wavlm_config, 
-                        #   "whisper": whisper_config}
-
-
-catalog = Config.model_catalog
-
-for key, value in catalog.items():
-    print(key)
-    processor = value["processor"]
-    model = value["model"]
-
-    for config in value["configs"]:
-        model = model.from_pretrained(config["model_config"], cache_dir="models/")
-
-# print(sum(p.numel() for p in model.parameters()))
